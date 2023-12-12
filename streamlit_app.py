6e7b7af3b59c19145686de0ec9cdc9f4b227d7b5
ryoukatajima <153050837+ryoukatajima@users.noreply.github.com>
2023-12-12 07:03:00 +0000

    おおお

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,13 @@
-# Streamlitライブラリをインポート
 import streamlit as st
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+# タイトルを追加
+st.title('朝ごはんを決める')
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+# 朝食の選択肢
+options = ['トースト', 'シリアル', 'ヨーグルト', 'フルーツ', 'オムレツ', 'パンケーキ', 'ベーコン＆エッグ']
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+# 選択肢から朝食を選ぶ
+selected_option = st.selectbox('今日の朝ごはんは？', options)
 
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
-
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
-
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
-
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+# 選択された朝食を表示
+st.write('選ばれた朝ごはん:', selected_option)
